098698869d61640b75b6f1f51cc24da02385ab13
Vijith-J-Shekha-P <vijith.shekha@gmail.com>
2025-02-23 12:43:08 +0530

    Delete src directory

--- src/lib/constants.js
+++
@@ -1,5 +0,0 @@
-export const baseURL = "http://localhost:3000/api/";
-export const tasksURL = "http://localhost:3000/api/tasks";
-export const userURL = "http://localhost:3000/api/user";
-export const loginURL = "http://localhost:3000/api/user/login";
-export const registerURL = "http://localhost:3000/api/user/register";
